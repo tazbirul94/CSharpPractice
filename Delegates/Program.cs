d879f7576bbad641c7f306b05b23c795e1164e15
MD Tazbirul Haque <tazbirul94@gmail.com>
2023-07-20 22:22:38 +0200

    initial

---
+++ Delegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using DemoLibrary;
+using System.Runtime.CompilerServices;
+
+ShoppingCartModel cart = new();
+
+PopulateCartWithDemoData();
+
+Console.WriteLine($"The total for the cart is {cart.GernerateTotal():C2}");
+
+//Console.WriteLine();
+Console.Write("Please press any key to exit the application...");
+Console.ReadKey();
+
+void PopulateCartWithDemoData()
+{
+    cart.Items.Add(new ProductModel { ItemName = "Cereal", Price = 3.63M });
+    cart.Items.Add(new ProductModel { ItemName = "Milk", Price = 2.95M });
+    cart.Items.Add(new ProductModel { ItemName = "Strawberries", Price = 7.51M });
+    cart.Items.Add(new ProductModel { ItemName = "Blueberries", Price = 8.84M });
+}
+
---
+++ DemoLibrary/ShoppingCartModel.cs
@@ -0,0 +1,31 @@
+﻿namespace DemoLibrary;
+
+public class ShoppingCartModel
+{
+    public delegate void MentionDiscount(decimal subTotal);
+    public List<ProductModel> Items { get; set; } = new();
+
+    public decimal GernerateTotal(MentionDiscount mentionDiscount)
+    {
+        decimal subTotal = Items.Sum(x => x.Price);
+
+        mentionDiscount(subTotal);
+
+        if (subTotal > 100)
+        {
+            return subTotal *= 0.80M;
+        }
+        else if (subTotal > 50)
+        {
+            return subTotal *= 0.85M;
+        }
+        else if (subTotal > 10)
+        {
+            return subTotal *= 0.90M;
+        }
+        else
+        {
+            return subTotal;
+        }
+    }
+}
