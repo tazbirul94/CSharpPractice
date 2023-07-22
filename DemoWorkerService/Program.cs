9251948665d33b13f4dc422be173c0d6c3b67f3a
MD Tazbirul Haque <tazbirul94@gmail.com>
2023-07-22 15:39:21 +0200

    Worker service added

---
+++ DemoWorkerService/Program.cs
@@ -0,0 +1,10 @@
+using DemoWorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
