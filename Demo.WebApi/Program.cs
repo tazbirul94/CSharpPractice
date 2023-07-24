d027b1ff8440f10643a94a4d9245bd7a1a6b1a64
MD Tazbirul Haque <tazbirul94@gmail.com>
2023-07-25 00:15:05 +0200

    docker image

--- Demo.WebApi/Program.cs
+++ Demo.WebApi/Program.cs
@@ -14,7 +14,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CSharpPractice v1"));
 }
 
 app.UseHttpsRedirection();
