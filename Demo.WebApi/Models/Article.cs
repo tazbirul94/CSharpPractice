6bf11f70f14ecee449e028e76733ebfbe3656486
MD Tazbirul Haque <tazbirul94@gmail.com>
2023-07-29 01:17:00 +0200

    model added

---
+++ Demo.WebApi/Models/Article.cs
@@ -0,0 +1,13 @@
+﻿namespace Demo.WebApi.Models;
+
+public class Article
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Author { get; set; }
+    public DateTime Created { get; set; } = DateTime.Now;
+    public DateTime Updated { get; set; } = DateTime.Now;
+    public bool IsDeleted { get; set; }
+
+}
